8ee812559dda415321ba6e1bd25d186257bba23f
Punit Yadav <punit965@gmail.com>
2025-12-25 00:41:08 +0530

    form style updated

--- controllers/joinClubGet.js
+++ controllers/joinClubGet.js
@@ -1,4 +1,9 @@
 module.exports = (req, res) => {
     const {id} = req.params
-    res.render('join-club', {id:id, user:req.user, title:'Join Private Club'})
+    if (req.isAuthenticated()) {
+        res.render('join-club', {id:id, user:req.user, title:'Join Private Club'})
+    } else {
+        res.redirect('/login')
+    }
+    
 }
--- controllers/newPostGet.js
+++ controllers/newPostGet.js
@@ -1,4 +1,8 @@
 module.exports = async (req, res) => {
-    const {id} = req.params;
-    res.render('new-post', {id: id, user:req.user, title:'Add New Post'})
-}
+  if (req.isAuthenticated()) {
+    const { id } = req.params;
+    res.render("new-post", { id: id, user: req.user, title: "Add New Post" });
+  } else {
+    res.redirect("/login");
+  }
+};
